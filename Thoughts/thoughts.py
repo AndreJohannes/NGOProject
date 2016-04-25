c8c515ff331bc97f26933c5713ee81f43c40d86c
Andre <stoffel.andre@gmail.com>
2016-04-24 20:41:12 -0500

    work in progress

--- Thoughts/thoughts.py
+++ Thoughts/thoughts.py
@@ -2,6 +2,8 @@ from PIL import Image
 from PIL import ImageDraw
 import aggdraw
 import math
+import random
+from phrases import Phrases
 
 class RCRectengular:
 	@staticmethod
@@ -72,33 +74,29 @@ class TimedBubble:
 
 class MorphingTextBox:
 	
-	def __init__(self, pos_x, pos_y, radius, startTime):
-		self.textbox = Image.new("L",(120,60),"black")
-		d = aggdraw.Draw(self.textbox)
-		font = aggdraw.Font("white", "font1.ttf",36)
-		d.text((10, 0), "Build a wall?", font)
-		d.text((30,25), "Really?", font)
-		d.flush()
+	def __init__(self, image, pos_x, pos_y, radius, startTime):
+		self.image = image
 		self.pos_x = pos_x
 		self.pos_y = pos_y
 		self.radius = radius
 		self.startTime = startTime
 		self.counter = 0
+		self.randomShift = random.randint(-10,10)
 
 	def draw(self, im):
 		if self.counter < self.startTime: 
 			return 
 		dt = self.counter  - self.startTime
 		y = self.pos_y - (dt)
-		x = self.pos_x - 10*math.sqrt((dt))
+		x = self.pos_x - self.randomShift*math.sqrt((dt))
 		radius = int(max(self.radius - dt / 2.,5))
-		hdx = min(int(self.radius + dt / 2.),(120+radius)/2)
-		hdy = min(int(self.radius + dt / 4.),(60+radius)/2)
+		hdx = min(int(self.radius + dt / 2.),(self.image.size[0]+radius)/2)
+		hdy = min(int(self.radius + dt / 4.),(self.image.size[1]+radius)/2)
 		d = aggdraw.Draw(im)
 		p = aggdraw.Pen("black", 2.5)
 		RCRectengular.draw(d, (x - hdx, y - hdy , x + hdx , y + hdy), radius, p)	
 		d.flush()
-		textbox = self.textbox.resize((2*hdx-2*radius,2*hdy-2*radius), Image.ANTIALIAS)
+		textbox = self.image.resize((2*hdx-2*radius,2*hdy-2*radius), Image.ANTIALIAS)
 		color = 255 - dt*5
 		im.paste((color,color,color,256),(int(x)-hdx+radius,y-hdy+radius),textbox.convert("L"))
 
@@ -107,11 +105,11 @@ class MorphingTextBox:
 
 class OneThought:
 
-	def __init__(self, startTime):
-		self.bubble1 = TimedBubble(380,358,10,startTime,startTime+105,30,5)
-		self.bubble2 = TimedBubble(380,318,15,startTime,startTime+105,20,15)
-		self.bubble3 = TimedBubble(380,268,20,startTime,startTime+105,10,25)
-		self.rectangualar = MorphingTextBox(380,268,20,startTime+105)
+	def __init__(self, startTime, image, pos_x):
+		self.bubble1 = TimedBubble(pos_x,358,10,startTime,startTime+105,30,5)
+		self.bubble2 = TimedBubble(pos_x,318,15,startTime,startTime+105,20,15)
+		self.bubble3 = TimedBubble(pos_x,268,20,startTime,startTime+105,10,25)
+		self.rectangualar = MorphingTextBox(image, pos_x,268,20,startTime+105)
 
 	def inc(self):
 		self.bubble1.inc()
@@ -127,21 +125,27 @@ class OneThought:
 
 base = Image.open("./base.png")
 
-iOffset = 0
-firstThought = OneThought(0)
-secondThought = OneThought(120)
+iOffset = 456
+firstThought = OneThought(0, Phrases.getPhrase1(),380)
+secondThought = OneThought(80, Phrases.getPhrase3(),945)
+thirdThought = OneThought(160, Phrases.getPhrase2(),380)
+forthThought = OneThought(240, Phrases.getPhrase4(),945)
 
-for i in range(0,350):
+for i in range(0,450):
 	im = Image.new("RGBA",(1280,720),"white")
 	im.paste(base,(0,0))
 	firstThought.draw(im)
 	secondThought.draw(im)
+	thirdThought.draw(im)
+	forthThought.draw(im)
 	d = ImageDraw.Draw(im)
 	d.text((0,0),str(i+iOffset),"black")
 	firstThought.inc()
 	secondThought.inc()
+	thirdThought.inc()
+	forthThought.inc()
 	print "saving image:" , i 
-	im.save("./images/image" + str(i+iOffset) + ".png","png")
-
+	#im.save("./images/image" + str(i+iOffset) + ".png","png")
+	im.save("../Movie/image" + str(i+iOffset) + ".png","png")
 
  	
--- WallBuilding/wall.py
+++ WallBuilding/wall.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw
 import aggdraw
 import math
 
-ioffset = 396
+iOffset = 396
 
 for i in range(0,30):
 	image = Image.open("./images/wall"+ "{:02d}".format(i) +".png")
@@ -12,5 +12,10 @@ for i in range(0,30):
 	p = aggdraw.Pen("black", 8.46666)
 	d.ellipse((640-315,596,640+315,596),p)
 	d.flush()
-	image.save("../Movie/image" + str(ioffset+2*i) +".png")
-	image.save("../Movie/image" + str(ioffset+2*i+1) +".png")
+	image1 = image.copy()
+	d = ImageDraw.Draw(image1)
+	d.text((0,0),str(2*i+iOffset),"black")
+	image1.save("../Movie/image" + str(iOffset+2*i) +".png")
+	d = ImageDraw.Draw(image)
+	d.text((0,0),str(2*i+iOffset+1),"black")
+	image.save("../Movie/image" + str(iOffset+2*i+1) +".png")
